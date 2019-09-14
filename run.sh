bf196acf504413765d4e08a56946da4fcc35f40f
JackJin96 <hanchong1996@hotmail.com>
2019-09-14 13:38:59 -0400

    update

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ find . -name '*.pyc' -delete
 
 export FLASK_APP=App
 export FLASK_ENV=development
-# remove when in development
-export OAUTHLIB_INSECURE_TRANSPORT=1
 
 echo $FLASK_APP
 echo $FLASK_ENV
